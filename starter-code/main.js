b14a0c45eab750d83f932755c897a45c888b9614
Matt Martinez <mattmartinez@Matts-MacBook-Pro.local>
2016-12-04 21:25:19 -0800

    Unit 10 changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -6,8 +6,20 @@ var cardThree = 'king';
 var cardFour = 'king';
 /* Four cards, two queens and two kings */
 
-if (cardTwo === cardFour) {
+/* if (cardTwo === cardFour) {
 	alert('You found a match!');
 } else {
 	alert('Sorry, try again.');
+} */
+
+var board = document.getElementById('game-board');
+
+function createCards() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
+
+		board.appendChild(cardElement);
+	}
 }

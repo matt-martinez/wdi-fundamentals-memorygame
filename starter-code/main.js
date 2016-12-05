a9a22f8bedeb55b4b9a24e5524526763547cb6e6
Matt Martinez <mattmartinez@Matts-MacBook-Pro.local>
2016-12-04 20:30:45 -0800

    Unit 9 Changes

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,8 +1,13 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cardOne = 'queen';
+var cardTwo = 'queen';
+var cardThree = 'king';
+var cardFour = 'king';
 /* Four cards, two queens and two kings */
 
+if (cardTwo === cardFour) {
+	alert('You found a match!');
+} else {
+	alert('Sorry, try again.');
+}
